7b73ff8a4ba9c891d042a2872022b4a0c4518b82
AlexMah123 <alexmahjy@gmail.com>
2025-01-25 17:46:43 +0800

    Behaviour Components, Refactor Enemy, DashBehaviour

---
+++ Assets/Scripts/Enemy Script/Behaviour/Scriptable/ProjectileShootBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Enemy_Script.Behaviour.Scriptable
+{
+    [CreateAssetMenu(fileName = "ProjectileShootBehaviour", menuName = "BehaviourScriptables/ProjectileShootBehaviour", order = 3)]
+    public class ProjectileShootBehaviour : BehaviourScriptable
+    {
+        public override void Setup(Enemy enemyRef, GameObject target)
+        {
+            base.Setup(enemyRef, target);
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+        
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void OnDeath()
+        {
+            base.OnDeath();
+        }
+    }
+}
